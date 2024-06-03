bcfac89adba654fca67b4007ca7320d0e1e374e2
Jonatan <Togo36918@gmail.com>
2024-06-03 16:57:54 -0300

    Nuevo tema

---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import { Write } from "./components/miPrimerComponente/Write";
+
+function App() {
+  return (
+    <>
+      <Write name={"Nacho"} lastName={"Torres"} />
+    </>
+  );
+}
+
+export default App;
---
+++ react/vite-project/src/components/miPrimerComponente/Write.jsx
@@ -0,0 +1,8 @@
+export function Write({ name, lastName }) {
+  return (
+    <>
+      Hola, mi nombre es {name} {lastName}
+      <img src="./imgs/chaufa.jpeg" alt="" />
+    </>
+  );
+}
