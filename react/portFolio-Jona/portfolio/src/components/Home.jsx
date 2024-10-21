b8bce8128e47933cb71c59c96180a3eaf7d9c8b3
Jonatan <togo36918@gmail.dom>
2024-10-21 20:02:40 -0300

    Incorpore una imagen la cual se uso en Home y se le agrego un poco más de texto, también se incorporo código en ´App.css´ para que el texto este a la izquierda y la imagen a la derecha

--- react/portFolio-Jona/portfolio/src/components/Home.jsx
+++ react/portFolio-Jona/portfolio/src/components/Home.jsx
@@ -2,11 +2,19 @@ import React from "react";
 
 const Home = () => {
   return (
-    <main className="orientation">
-      <h1>Bienvenido a Mi Portafolio</h1>
+    <main className="orientation" >
+      <div className="home" id="home">
+      <div className="home-content">
+      <h1>hola!, Bienvenido a Mi Portafolio.</h1>
+      <h3>Mi nombre es Jonatan Vilte</h3>
       <p>
-        Aquí encontrarás información sobre mis proyectos y cómo contactarme.
+        Aquí encontrarás información sobre mis proyectos como mis habilidades, pryectos y contacto.
       </p>
+      </div>
+      <div className="home-img">
+        <img src="./src/imagenes/gatoBanana.jpg" alt="gato" />
+      </div>
+      </div>
     </main>
   );
 };
