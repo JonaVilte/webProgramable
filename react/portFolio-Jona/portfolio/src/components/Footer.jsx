4ce289ddd2618bbd2ef8e1978f19539619922454
Jonatan <togo36918@gmail.dom>
2024-10-21 23:26:13 -0300

    2 imagenes fueron agregadas al ´footer´ las cuales tienen link a dos paginas, tambien se agrego css para que se viera mejor y para que al pasar por encima de los iconos haga un pequeño zoom

--- react/portFolio-Jona/portfolio/src/components/Footer.jsx
+++ react/portFolio-Jona/portfolio/src/components/Footer.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; 2024 Mi Portafolio. Todos los derechos reservados.</p>
-      <img src="" alt="" />
+    <footer className="footer">
+      <div className="social-links">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <img className="icono-img skill-item" src="./src/imagenes/twitter.png" alt="" />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <img className="icono-img skill-item" src="./src/imagenes/instagram.png" alt="" />
+        </a>
+        <p>Numero tel: 11 5964-4380</p>
+        <p>&copy; {new Date().getFullYear()} Tu Nombre. Todos los derechos reservados.</p>
+
+      </div>
     </footer>
   );
 };
